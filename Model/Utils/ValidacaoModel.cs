2a8a638f5407318493c977b2807bc8d0fef5bad6
robertodecampos <roberto.rfc@live.com>
2019-03-23 12:04:08 -0300

    Adicionando classe para validação das classes Model

---
+++ Model/Utils/ValidacaoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Utils
+{
+    public abstract class ValidacaoModel
+    {
+        public abstract bool IsValid(out string[] mensagens);
+
+        public bool IsValid(out string mensagem)
+        {
+            string[] mensagens;
+
+            mensagem = "";
+
+            if (IsValid(out mensagens))
+                return true;
+            else
+            {
+                mensagem = mensagens[0];
+                return false;
+            };
+        }
+
+        public bool IsValid()
+        {
+            string[] mensagens;
+
+            return IsValid(out mensagens);
+        }
+    }
+}
