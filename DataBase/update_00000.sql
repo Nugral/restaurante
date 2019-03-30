72a0ba3e14e0b90da0adf7b9f762b5d7cdcef0ac
robertodecampos <roberto.rfc@live.com>
2019-03-30 12:34:59 -0300

    Adicionando campo para armazenar senha do funcionário

--- DataBase/update_00000.sql
+++ DataBase/update_00000.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `restaurante`.`funcionario`   
-  ADD COLUMN `nivel` VARCHAR(30) NULL AFTER `cpf`;
+  ADD COLUMN `nivel` VARCHAR(30) NULL AFTER `cpf`,
+  ADD COLUMN `senha` CHAR(32) NULL AFTER `nivel`;
