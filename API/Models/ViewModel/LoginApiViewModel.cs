2aee78b0e6fa30abd9a7a43dc1727de2cd22eaf2
robertodecampos <roberto.rfc@live.com>
2019-04-04 21:43:04 -0300

    Solicitando Cpf e Senha do funcionário

--- API/Models/ViewModel/LoginApiViewModel.cs
+++ API/Models/ViewModel/LoginApiViewModel.cs
@@ -4,5 +4,7 @@
     {
         public string Usuario { get; set; }
         public string Senha { get; set; }
+        public string Cpf { get; set; }
+        public string SenhaFuncionario { get; set; }
     }
 }
